1168554c37ae9b85fd77689dc2b50b337b682aba
Fernanda Esquivel <80931098+FerEsq@users.noreply.github.com>
2025-09-09 23:05:35 -0600

    [feat] file creation

---
+++ textManipulation.py
@@ -0,0 +1,8 @@
+'''
+ * Nombre: textManipulation.py
+ * Programadora: Fernanda Esquivel (esq21542@uvg.edu.gt)
+ * Lenguaje: Python
+ * Historial: 
+    - Creado el 09.09.2025
+    - Modificado el 09.09.2025
+'''
