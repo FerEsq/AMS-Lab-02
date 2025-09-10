60b56f3f9bc00322754ef8f8aee8ed7f0af35186
Fernanda Esquivel <80931098+FerEsq@users.noreply.github.com>
2025-09-09 23:35:34 -0600

    [feat] unit testing file created

---
+++ test_textManipulation.py
@@ -0,0 +1,9 @@
+'''
+ * Nombre: test_textManipulation.py
+ * Programadora: Fernanda Esquivel (esq21542@uvg.edu.gt)
+ * Descripción: Pruebas unitarias para todas las funciones de manipulación de texto
+ * Lenguaje: Python
+ * Historial: 
+    - Creado el 09.09.2025
+    - Modificado el 09.09.2025
+'''
