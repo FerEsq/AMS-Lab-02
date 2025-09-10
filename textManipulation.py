aaaa9bb5ef457ca089765fbd17fa5185b665ef2b
Fernanda Esquivel <80931098+FerEsq@users.noreply.github.com>
2025-09-09 23:19:26 -0600

    [feat] reverse function

--- textManipulation.py
+++ textManipulation.py
@@ -1,8 +1,25 @@
 '''
  * Nombre: textManipulation.py
  * Programadora: Fernanda Esquivel (esq21542@uvg.edu.gt)
+ * Descripción: Librería para realizar operaciones comunes de manipulación de cadenas de texto
  * Lenguaje: Python
  * Historial: 
     - Creado el 09.09.2025
     - Modificado el 09.09.2025
 '''
+
+'''
+Retorna una cadena de texto en orden inverso
+
+Args:
+    s (str): La cadena de texto a invertir
+    
+Returns:
+    str: La cadena invertida
+'''
+def reverse(s):
+
+    if not isinstance(s, str):
+        raise TypeError("El parametro debe ser una cadena de texto")
+    
+    return s[::-1]
